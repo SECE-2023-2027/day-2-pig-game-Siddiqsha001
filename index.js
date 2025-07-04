72fec0b5fabbbf54e7688d16fc7e8d12d4c4aa47
Siddiqsha <156308269+Siddiqsha001@users.noreply.github.com>
2025-07-04 09:53:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
